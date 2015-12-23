3ca4a94444778e784aa84316943c48a843354498
lxj616 <502307678@qq.com>
2015-12-23 18:18:14 +0800

    Update config.inc.php

--- config/config.inc.php
+++ config/config.inc.php
@@ -17,7 +17,7 @@ $_DVWA = array();
 $_DVWA[ 'db_server' ] = 'localhost';
 $_DVWA[ 'db_database' ] = 'dvwa';
 $_DVWA[ 'db_user' ] = 'root';
-$_DVWA[ 'db_password' ] = '';
+$_DVWA[ 'db_password' ] = 'p@ssw0rd';
 
 # Only needed for PGSQL
 $_DVWA[ 'db_port' ] = '5432'; 
